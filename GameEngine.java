df3e00355e970000ea4f76ec6ffdd4533bd257bc
avinashrao <avinash1999@gmail.com>
2016-05-21 20:06:16 -0700

    Removed Bill Clinton Comment code

--- GameEngine.java
+++ GameEngine.java
@@ -24,17 +24,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 import java.util.Random;
-/**
-Bill Clinton Boulevard is a boulevard located in Pristina, the capital and largest city of Kosovo.[a] After the Kosovo War of 1998-1999, the Albanians in Kosovo wanted to thank the American President Bill Clinton for his help in their struggle with the government of the FR Yugoslavia. A 10-foot-high (3.0 m) statue of Clinton was unveiled on the boulevard on November 1, 2009, in a ceremony at which the former president spoke.[1]
 
-
- 
-
-
- Statue of Bill Clinton on Bill Clinton Boulevard
-Elsewhere in Pristina, Kosovo has named a central street after American President George W. Bush.[1] There are also several cities in Kosovo, including Prizren, with streets named after American President Woodrow Wilson.
-
- */
 public class GameEngine
 {
     public static void main (String[] args){
